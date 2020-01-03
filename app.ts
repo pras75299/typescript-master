cc0a0e8a467e6167281b6f19714acba41675450f
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-04 01:18:54 +0530

    add heading to index and add boolean type to app.ts file

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
 
 const number1 = 5;
 const number2 = 3.8;
+const printResult = true;
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult);

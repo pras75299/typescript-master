11439c860289821f8c8b647c2dde148b06bf95e4
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-04 01:23:50 +0530

    customize result by adding a string

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, pharse: string) {
+  const result = n1 + n2;
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(pharse + result);
   } else {
-    return n1 + n2;
+    return result;
   }
 }
 
 const number1 = 5;
 const number2 = 3.8;
 const printResult = true;
+const resultPharse = "Result is: ";
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPharse);

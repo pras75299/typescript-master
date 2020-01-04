158e40a3502df4b7f28cc70848be7f14cbe12e41
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-05 00:48:57 +0530

    alias union types

---
+++ union-aliasestype.ts
@@ -0,0 +1,17 @@
+type Combinable = number | string;
+
+function combine(input1: Combinable, input2: Combinable) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combineAges = combine(30, 26);
+console.log(combineAges);
+
+const combineNames = combine("max", "pahadi");
+console.log(combineNames);

002db60653025be1f50cab32fb311b2fd47cece1
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-04 23:47:29 +0530

    creating custom types union and literal types examples

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+
+function combine(input1: Combinable, input2: Combinable) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;

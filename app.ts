9af71de4f024a9451317ee3b1863b54567f55ff3
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-04 22:57:07 +0530

    Tuple example in typescript

--- app.ts
+++ app.ts
@@ -1,7 +1,17 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Prashant",
   age: 30,
-  hobbied: ["Sports", "Cooking"]
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "NoneABC"]
 };
+
+//person.role.push('Admin'); //this is an exception for Tuples !! should aware of !!
+
+//person.role = [2, "abc", 15]; // although gives support here by not, letting pas 3rd argument
 
 console.log(person.name);

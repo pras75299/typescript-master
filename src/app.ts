28d250df73b4727f30f4d324f35576037ff29833
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-08 01:17:04 +0530

    spread operators in object

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,12 @@ const activeHobbies = ["Hiking", ...hobbies];
 
 //activeHobbies.push(hobbies[0], hobbies[1]); //looking cumbersome
 activeHobbies.push(...hobbies);
+
+const personSS = {
+  name: "Vikas",
+  age: 29
+};
+
+const copiedPerson = { ...personSS }; //with this we have a exact copy of personSS object
+copiedPerson.name = "new Delhi";
+console.log(copiedPerson);

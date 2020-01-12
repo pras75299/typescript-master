7b90ec455dcf1c79e5bf58beeab787691c634a29
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-12 13:02:58 +0530

    working with classes

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,9 @@
-const hobbies = ["Sports", "cooking"];
-const activeHobbies = ["Hiking", ...hobbies];
+class Department {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+}
 
-//activeHobbies.push(hobbies[0], hobbies[1]); //looking cumbersome
-activeHobbies.push(...hobbies);
-
-const personSS = {
-  name: "Vikas",
-  age: 29
-};
-
-const copiedPerson = { ...personSS }; //with this we have a exact copy of personSS object
-copiedPerson.name = "new Delhi";
-console.log(copiedPerson);
-
-const addRest = (...numbersRest: number[]) => {
-  return numbersRest.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
-
-const addNumbersRest = addRest(1, 2, 3, 4, 5, 6, 7);
-console.log(addNumbersRest);
+const accounting = new Department("Accounting");
+console.log(accounting);

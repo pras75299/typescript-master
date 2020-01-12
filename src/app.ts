2390952c09e471838cb27cbcea7fb04b03c175dc
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-12 13:37:35 +0530

    add class and this concept

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,15 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
 const accounting = new Department("Accounting");
-console.log(accounting);
+
+accounting.describe();
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+accountingCopy.describe();

93e055fdf657b6780cfb151b5059f9d45ee915d7
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-14 22:21:27 +0530

    working with classes and inheritance with extends keyword and super method

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,53 @@
 class Department {
-  name: string;
-  constructor(n: string) {
-    this.name = n;
+  //public name: string;
+  private employees: string[] = [];
+  constructor(private readonly id: string, public name: string) {
+    //this.name = n;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
+}
 
-accounting.describe();
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-accountingCopy.describe();
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["MAX"]);
+it.addEmployee("MAX");
+it.addEmployee("MANU");
+
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
+
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReport("something went wrong..");
+accounting.printReports();
+
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();

41351a34b392d4e3c0a049340e554ff811c84106
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-15 23:20:31 +0530

    working with interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("working with interfaces");
+interface Greetable {
+  name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+let user1: Greetable;
+user1 = {
+  name: "Max",
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+};
+
+user1.greet("Hi there - I am");
+console.log(user1);

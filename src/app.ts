e255d940417abf166d358be67e381b27ff940c00
prashant kumar singh <singh.prashantking@gmail.com>
2020-01-31 01:16:45 +0530

    type guard typescript

--- src/app.ts
+++ src/app.ts
@@ -67,3 +67,27 @@ function useVehical(vehical: Vehical) {
 
 useVehical(v1);
 useVehical(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving at speed: " + speed);
+}
